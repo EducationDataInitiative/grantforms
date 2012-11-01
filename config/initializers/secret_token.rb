7495759c292a9a4c5f8b55576295ced7af442a0b
Marina Martin <git@marinamartin.com>
2012-11-01 14:02:07 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grants::Application.config.secret_token = 'b53719bca0ac3cc3a5e21437edfd0d0ff03630f2ad44bc706be107bb75f2d561f42a838c831af4cfb4c56cb52b94fb2b5621bc5cb30f8c746ecb4a260f5e8616'
