c06c11f852afce9232b9bf820b06f0e7070f4310
Marina Martin <marina.martin@ed.gov>
2012-11-01 15:19:58 -0400

    Removed password confirmation requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :omniauthable, :registerable, :recoverable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :remember_me
   # attr_accessible :title, :body
 end
