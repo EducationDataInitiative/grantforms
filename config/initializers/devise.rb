9e6d3e626170b00b84d0bcffb6572f6823aa76cf
adamjacobbecker <ad@mbecker.cc>
2013-01-09 10:33:36 -0500

    use MYGOV_HOME global var

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -197,7 +197,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :mygov, MYGOV_CLIENT_ID, MYGOV_SECRET_ID, :scope => 'profile,notifications,tasks'
+  config.omniauth :mygov, MYGOV_CLIENT_ID, MYGOV_SECRET_ID, :scope => 'profile,notifications,tasks',
+                                                            :client_options => {:site => MYGOV_HOME,
+                                                                                :token_url => "/oauth/authorize"}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
